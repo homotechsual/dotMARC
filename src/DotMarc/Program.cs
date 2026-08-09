2762552b9675e45610f64c557cdd7abb04a36b5d
Mikey O'Toole <mikey@homotechsual.dev>
2026-08-09 21:58:02 +0100

    Scaffold dotMARC solution: Blazor Server + MudBlazor, empty test project

---
+++ src/DotMarc/Program.cs
@@ -0,0 +1,22 @@
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddMudServices();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<DotMarc.Components.App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
