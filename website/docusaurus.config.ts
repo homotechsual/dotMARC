31cf34e99676afa357849468ae2f59cbaaa449f4
Mikey O'Toole <mikey@homotechsual.dev>
2026-09-02 00:28:05 +0100

    Update Yarn to 4.18 and fix import of FeatureRequests plugin.

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -3,7 +3,6 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 import plausiblePlugin from '@homotechsual/docusaurus-plugin-plausible';
 import type {PluginOptions as PlausiblePluginOptions} from '@homotechsual/docusaurus-plugin-plausible';
-import FeatureRequestsPlugin from './src/plugins/FeatureRequestsPlugin';
 
 const {docs: docsOgRenderer, pages: pagesOgRenderer, blog: blogOgRenderer} = require('./lib/ImageRenderers.cjs');
 const ogPlugin = require('@homotechsual/docusaurus-og');
@@ -147,7 +146,7 @@ const config: Config = {
       darkTheme: prismThemes.dracula,
     },
     plugins: [
-      FeatureRequestsPlugin,
+      './src/plugins/featureRequests/FeatureRequestsPlugin.js',
     ],
   } satisfies Preset.ThemeConfig,
 };
